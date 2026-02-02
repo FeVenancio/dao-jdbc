32ca4cedabbb33dbfc5ce64ff1c4b3e5205cf1ad
FeVenancio <fvenancio444@gmail.com>
2026-02-02 15:23:45 -0300

    Seller Entities Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
     public static void main (String[] args) {
@@ -13,7 +15,12 @@ public class Program {
         Scanner scn = new Scanner(System.in);
 
         Department obj = new Department(1, "books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+
         System.out.println(obj);
+
+        System.out.println(seller);
+
         DB.getConnection();
 
         scn.close();
