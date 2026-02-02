81beb59b516ebb6fb31f82c9a2237f2c1f5c2249
FeVenancio <fvenancio444@gmail.com>
2026-02-02 15:45:19 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,8 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import db.DB;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -16,6 +18,8 @@ public class Program {
 
         Department obj = new Department(1, "books");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+        
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(obj);
 
